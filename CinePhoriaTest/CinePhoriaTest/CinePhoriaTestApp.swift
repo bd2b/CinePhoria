4d03a49deff4256e251beb089800c87863a01177
bgr9 <bgr.publi@laposte.net>
2024-11-23 09:16:43 +0100

    Deuxieme Commit avec XCode

---
+++ CinePhoriaTest/CinePhoriaTest/CinePhoriaTestApp.swift
@@ -0,0 +1,19 @@
+//
+//	CinePhoriaTestApp.swift
+//	CinePhoriaTest
+//
+//  Cree par Bruno DELEBARRE-DEBAY on 23/11/2024.
+//  bd2db
+//
+
+
+import SwiftUI
+
+@main
+struct CinePhoriaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinePhoriaTest/CinePhoriaTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//	ContentView.swift
+//	CinePhoriaTest
+//
+//  Cree par Bruno DELEBARRE-DEBAY on 23/11/2024.
+//  bd2db
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
