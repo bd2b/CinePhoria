8bf3c1cd1d213afd42887a8cfaf6eb093d58aa29
bd2b <bruno.d2b@mail.fr>
2024-12-02 14:50:18 +0100

    core : Création de la target de test MonCinePhoriaTests , test présence assets AssetsTests

--- CinePhoria mobile/MonCinePhoriaTests/AssetsTests.swift
+++ CinePhoria mobile/MonCinePhoriaTests/AssetsTests.swift
@@ -8,11 +8,55 @@
 
 
 import Testing
+import MonCinePhoria
+import SwiftUI
 
 struct AssetsTests {
+    @Test(arguments: [
+        "Argenté (tertiaire)",
+        "Blanc Casse (secondaire)",
+        "Bleu nuit (primaire)",
+        "Doré (accentuation)",
+        "Gris perle (fond)",
+        "Rouge sombre (erreur)",
+        "Vert foncé (succes)"
+    ])
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    func testColors(colorName: String) async throws {
+        #expect((UIColor(named: colorName) != nil), "Couleur UI \(colorName) existe")
+    }
+    
+    @Test(arguments:
+            [
+        "camera-cinephoria-fd-blanc-login",
+        "camera-cinephoria-fd-blanc-login-trous trnsp",
+        "camera-cinephoria2",
+        "camera-cinephoria3"
+        ]
+    )
+    func testFileGif(nameFile: String) async throws {
+        #expect(Bundle.main.path(forResource: nameFile, ofType: "gif") != nil, "Fichier \(nameFile).gif existe")
+    }
+    
+    @Test(arguments:
+            [
+        "Actor-Regular",
+        "LCD14"
+        ]
+    )
+    func testFileFont(nameFile: String) async throws {
+        #expect(Bundle.main.path(forResource: nameFile, ofType: "ttf") != nil, "Fichier \(nameFile).ttf existe")
+    }
+    
+    @Test(arguments:
+            [
+                "3D",
+                "4DX",
+                "4K"
+        ]
+    )
+    func testFileImage(nameFile: String) async throws {
+        #expect(Image(nameFile) != nil, "Image \(nameFile)")
     }
 
 }
