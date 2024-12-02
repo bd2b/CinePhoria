e7e2fe268e885bbdaa36ea01779831d964f6feca
bd2b <bruno.d2b@mail.fr>
2024-12-02 14:48:05 +0100

    chore: paramétrage de la target MonCinePhoriaTest , AssetsTests.swift verifie les fichiers présents

---
+++ CinePhoria mobile/MonCinePhoriaTests/AssetsTests.swift
@@ -0,0 +1,18 @@
+//
+//	AssetsTests.swift
+//	MonCinePhoriaTests
+//
+//  Cree par Bruno DELEBARRE-DEBAY on 02/12/2024.
+//  bd2db
+//
+
+
+import Testing
+
+struct AssetsTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
